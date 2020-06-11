33c93e826b964887057dfe87f434735eee637357
User-Niomic-Fikri <fikrifauzan17@gmail.com>
2020-06-11 18:30:33 +0700

    Tugas 12 Javascript
    tuags

---
+++ tugas12.js
@@ -0,0 +1,10 @@
+function tugas12() {
+  var buah = ['Pisang','Jeruk','Apel','Mangga'];
+  console.log(buah);
+  buah.shift();
+  console.log(buah);
+  buah.pop();
+  return buah;
+
+}
+console.log(tugas12());
